f926c887d220049773e22f5e7dc87d442b5f9ae2
farland <farland@163.com>
2024-03-05 08:47:32 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Savvy, nextjs & tailwindcss blog.
+
+forked from [nextjs-blog](https://github.com/timlrx/tailwind-nextjs-starter-blog)
+
+## Customize
+
+- change fonts
+- change theme color;
+- change layout to simple layout
---
+++ data/projectsData.ts
@@ -0,0 +1,25 @@
+interface Project {
+  title: string,
+  description: string,
+  href?: string,
+  imgSrc?: string,
+}
+
+const projectsData: Project[] = [
+  {
+    title: 'soundme',
+    description: `Sync bbc sound programm playlist, and play the songs from netease music`,
+    imgSrc: '/static/images/google.png',
+    href: 'https://bbc.farland.vip',
+  },
+  {
+    title: 'The Time Machine',
+    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
+    to the desired date and press "Go". No more worrying about lost keys or
+    forgotten headphones with this simple yet affordable solution.`,
+    imgSrc: '/static/images/time-machine.jpg',
+    href: '/blog/the-time-machine',
+  },
+]
+
+export default projectsData
