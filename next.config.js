27e19d647ab0f8ef2131bc4eebf3f5310857a253
farland <farland@163.com>
2024-03-06 05:46:45 +0000

    fix font src

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const ContentSecurityPolicy = `
   img-src * blob: data:;
   media-src *.s3.amazonaws.com;
   connect-src *;
-  font-src 'self';
+  font-src  https://cdn.jsdelivr.net;
   frame-src giscus.app
 `
 
